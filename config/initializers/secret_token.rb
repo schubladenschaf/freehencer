20d968e90df5a9dcb468f275ccb2588f4af4bfe8
Jens Neuhaus <me@jensneuhaus.de>
2011-05-09 23:15:12 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freehencer::Application.config.secret_token = 'c0bfb65d26654824d67975593472e184fbc18b56d02214f2250cb17dc1f0394f6abe882c62871ebbd6d0f0ded8441a31530bcdea042d4178580d0294af7d6184'
